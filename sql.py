9809e733bf02ab689dc54a50b4c27948fba787a7
dgty <diguiotto.yann@gmail.com>
2016-08-22 13:15:42 +0000

    SQL Select and Insert requests python

---
+++ sql.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import datetime
+import MySQLdb
+
+conn = MySQLdb.connect(host= "31.220.17.165",
+user="dgtysoft_adm",
+passwd="db1743",
+db="dgtysoft_rpis")
+x = conn.cursor()
+
+name = "RPI3"
+ip = "ok"
+date = datetime.datetime.now()
+
+sql = ("SELECT Name FROM IPAddresses")
+result = x.execute(sql)
+for row in x.fetchall() :
+	print row
+
+query = """INSERT INTO IPAddresses (Name,IP,Date) VALUES ('%s','%s','%s')""" % (name,ip,date)
+
+print query
+
+try:
+	x.execute(query)
+	conn.commit()
+except:
+	conn.rollback()
+	
+	print "didn't work"
+
+conn.close()
